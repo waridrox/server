fa1360fc4cba03118cc9d92d7e1e7c142d911225
waridrox <mohdwarid4@gmail.com>
2021-12-08 19:49:05 +0530

    Separating models, planet controller and router

---
+++ src/routes/planets/planets.controller.js
@@ -0,0 +1,9 @@
+const planets = require('../../models/planets.model')
+
+function getAllPlanets (req, res) {
+    return res.status(200).json(planets) //for the error that the headers have already been set.
+}
+
+module.exports = {
+    getAllPlanets,
+}
