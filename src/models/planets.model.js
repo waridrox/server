f8d4d1418b32833340ecf31847792921f2373613
waridrox <mohdwarid4@gmail.com>
2021-12-09 11:22:43 +0530

    Fetching planets model asynchronous

--- src/models/planets.model.js
+++ src/models/planets.model.js
@@ -1,3 +1,47 @@
-const planets = []
+const fs = require('fs');
+const path = require('path')
+const parse = require('csv-parse');
 
-module.exports = planets
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+    return (planet['koi_disposition'] === 'CONFIRMED' 
+    && planet['koi_insol'] > 0.36 
+    && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6)
+}
+
+
+function loadPlanetsData() {
+    return new Promise ((resolve, reject) => {
+        //wait for the streaming code to resolve
+        fs.createReadStream(path.join( __dirname, '..', '..', 'data', 'kepler_data.csv'))
+        .pipe(parse({
+            comment: '#',
+            columns: true,
+        }))
+        .on('data', (data) => {
+            if (isHabitablePlanet(data)) {
+                habitablePlanets.push(data)
+            }
+        })
+        .on('error', (err) => {
+            console.log(err);
+            reject(err)
+        })
+        .on('end', () => {
+            // console.log(habitablePlanets.map((planet) => {
+            //     return planet['kepler_name']
+            // })); 
+            
+            //we don't need this since we are just setting the planets info data
+            console.log(`${habitablePlanets.length} habitable planets found!`);
+        })
+        resolve(); //signals that the promise has been resolved. 
+    })
+}
+
+module.exports = {
+    loadPlanetsData,
+    planets: habitablePlanets,
+}
--- src/routes/planets/planets.controller.js
+++ src/routes/planets/planets.controller.js
@@ -1,4 +1,4 @@
-const planets = require('../../models/planets.model')
+const { planets } = require('../../models/planets.model')
 
 function getAllPlanets (req, res) {
     return res.status(200).json(planets) //for the error that the headers have already been set.
